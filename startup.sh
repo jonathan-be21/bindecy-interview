40617135d99589bfc10ead074bb7ea8f7028605d
jonathanbe <yoni.benisty21@gmail.com>
2023-08-26 22:39:52 +0000

    changes

--- startup.sh
+++ startup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-docker build -t jenkins_master:latest -f master.dockerfile .
-docker build -t jenkins_agent:latest -f agent.dockerfile .
+### in order to run this script, please make sure you've installed docker
+read -p "Do you wish to build this project and run Jenkins master on the current platform? (Yes/No): " answer
 
-docker run --name jenkins_master -d -v ./jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins/jenkins
+if [[ "$answer" != "Yes" && "$answer" != "yes" ]] ;then 
+    exit 0
+fi
+
+docker build -t jenkins-master -f master.dockerfile .
+docker build -t jenkins-agent -f agent.dockerfile .
+
+docker run --name jenkins-master -d -v ./jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins-master
+
+### If you're running the jenkins master docker container on remote server
+### please use the following ssh tunnel command (replace USER and JENKINS_HOST with relevant details):
+### ssh USER@JENKINS_HOST -L 8080:JENKINS_HOST:8080
+
+echo "Jenkins Master login details:"
+echo "USERNAME: Admin"
+echo "Password: Aa123456"
